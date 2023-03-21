a9c8d7b6f30b0715a5c2feda858b2f395a9720e9
LDeive505 <luisdaviab@gmail.com>
2023-03-20 22:52:30 -0300

    add: AMC-ICPCteam solution

---
+++ ACM-ICPCTeam.js
@@ -0,0 +1,42 @@
+/*
+=================== PROBLEM DESCRIPTION =========================================================
+
+Problem Name: ACM ICPC Team
+Problem Link: https://www.hackerrank.com/challenges/acm-icpc-team/problem?isFullScreen=true
+HackerHanck Difficulty: Easy
+My Difficulty: Medium 1
+
+*/ 
+
+function acmTeam(topic) {
+  let maxTopics = 0;
+  let teamsCount = 0;
+
+    topic.forEach((t1) => {
+      topic.forEach((t2)=> {
+        const count = countMergedTopics(t1,t2);
+        if (count > maxTopics) {
+          maxTopics = count;
+          teamsCount = 1;
+        }
+        else if (count === maxTopics) teamsCount += 1;
+      });
+    });
+
+  return [maxTopics, teamsCount/2];
+}
+
+function countMergedTopics(t1,t2) {
+  let count = 0;
+  for(let i = 0 ; i < t1.length; i++)
+    if(t1[i]=== '1' || t2[i] === '1') count += 1;
+
+  return count;
+}
+
+// ==================== TESTS ======================
+
+let x = [ '10101', '11100', '11010', '00101' ];
+
+let res = acmTeam(x);
+console.log(res);
